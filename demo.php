cd08cbbb59e676cc3ece320500458a522da7a7c7
fanrehai <fanrehai@qq.com>
2019-12-15 22:59:04 +0800

    完成了数组的基本操作

--- demo.php
+++ demo.php
@@ -8,4 +8,46 @@ require_once './vendor/autoload.php';
 
 use Showapi\ApiDoc;
 
-ApiDoc::hello();
+$apiKey   = '7d337d34b91ea764e7004c906b6906da1377722134';
+$apiToken = 'a5933feb713545b71239c5bd0e1e2cc1357245110';
+$apiUrl   = 'https://showdoc.fanrehai.top/server/index.php?s=/api/item/updateByApi';
+$projectUrl = 'www.baidu.com';
+$api      = new ApiDoc($apiKey, $apiToken, $apiUrl, $projectUrl);
+
+//$api->say();
+//var_dump($api->apiKey);
+$data = [
+    "url"     => "https://qqe2.com",
+    "name"    => "欢迎使用JSON在线解析编辑器",
+    "array"   => [
+        "JSON"   => "http://jsonlint.qqe2.com/",
+        "Cron生成" => "http://cron.qqe2.com/",
+        "JS加密解密" => "http://edit.qqe2.com/asdasd"
+    ],
+    "boolean" => true,
+    "null"    => null,
+    "number"  => 123,
+    "object"  => [
+        "ac" => [
+            [
+                "JSON1"  => "http://jsonlint.qqe2.com/",
+                "Cron生成" => "http://cron.qqe2.com/",
+                "JS加密解密" => "http://edit.qqe2.com/"
+            ],
+            [
+                "JSON2"  => "http://jsonlint.qqe2.com/",
+                "Cron生成" => "http://cron.qqe2.com/",
+                "JS加密解密" => "http://edit.qqe2.com/"
+            ]
+        ],
+        "c" => "d",
+        "e" => "f"
+    ]
+];
+
+//$data = '{"id":"asd_888","controller":"asd","action":"888","method":"GET","params":["name","phone"]}';
+
+//$api->saveApiToLog( 'asd', 'asddddd', $data );
+//$api->saveApiToLog( 'asd', '888', ['name', 'phone', 'asd'] );
+
+$api->saveApiToWeb('asd', '888', $data);
--- src/ApiDoc.php
+++ src/ApiDoc.php
@@ -3,41 +3,300 @@
 # @Date:   2019-11-28 14:33:52
 # @Filename: ApiDoc.php
 # @Last modified by:   fanrehai
-# @Last modified time: 2019-11-28 16:03:46
+# @Last modified time: 2019-11-30 20:42:31
 
 namespace Showapi;
 
 Class ApiDoc
 {
-    public function __construct(){
+    /**
+     * Showdoc接口可以
+     */
+    private $apiKey;
+    /**
+     * Showdoc接口token
+     */
+    private $apiToken;
+    /**
+     * Showdoc接口写入地址
+     */
+    private $apiUrl;
+    /**
+     * 项目API访问地址
+     */
+    private $projectUrl;
 
+//    public function __set($name, $value = null)
+//    {
+//        if(isset($this->$name))
+//        {
+//            throw new \InvalidArgumentException("参数不存在");
+//        }
+//        $this->$name = $value;
+//    }
+////
+//    public function __get($name)
+//    {
+//        if(!isset($this->$name))
+//        {
+//            throw new \InvalidArgumentException("参数不存在");
+//        }
+//        return $this->$name;
+//    }
+
+    public function __construct($apiKey, $apiToken, $apiUrl, $projectUrl){
+         $this->apiKey   = $apiKey;
+         $this->apiToken = $apiToken;
+         $this->apiUrl   = $apiUrl;
+         $this->projectUrl = $projectUrl;
+//        var_dump($config);
     }
-    /**
-     * ShowAPI设置获取
-     */
-    public $apiKey;
-    /**
-     * ShowAPI设置获取
-     */
-    public $apiToken;
 
-    public static function hello()
+//$method = "PUT";
+//$varName = "_{$method}";
+//$_SERVER['REQUEST_METHOD'] === $method
+//    ? parse_str(file_get_contents('php://input', false , null, -1 , $_SERVER['CONTENT_LENGTH'] ), $$varName)
+//    : $$varName = [];
+//
+//var_dump($_PUT);
+//var_dump($_DELETE);
+//foreach ($_SERVER as $name => $value)
+//{
+//if (substr($name, 0, 5) == 'HTTP_')
+//{
+//$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+//}
+//}
+//parse_str(file_get_contents('php://input'), $adata);
+
+    /**
+     * @param $controllerName
+     * @param $actionName
+     * @param string $method 传输方式 Get Post Put Delete Patch
+     * @param $apiParams
+     */
+    public function saveApiToLog($controllerName, $actionName, $apiParams = [], $apiDesc = '暂无描述')
     {
-        echo "hello world";
+        if(!is_string($controllerName) || !is_string($actionName)){
+            throw new \InvalidArgumentException("controllerName and actionName参数必须是字符串类型");
+        }
+        if(!is_array($apiParams)){
+            throw new \InvalidArgumentException("apiParams参数不是一个有效数组");
+        }
+        $actionIds = $controllerName.'_'.$actionName;
+        $fileContent = self::fileContentReadHandle();
+        $isHandle = 0;
+        foreach ($fileContent as &$v) {
+            if(!isset($v['id'])){
+                continue;
+            }
+            if($v['id'] == $actionIds && $v['params'] == $apiParams){
+                $isHandle = 1;
+                break;
+            }
+        }
+        if($isHandle == 1){
+            return ;
+        }
+        // 获取参数传输方式
+        $method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+        $writeContent = [
+            'id'         => $actionIds,
+            'controller' => $controllerName,
+            'action'     => $actionName,
+            'method'     => $method,
+            'params'     => $apiParams,
+            'desc'       => $apiDesc
+        ];
+        self::fileContentWriteHandle($writeContent);
     }
 
-    public function showdocApi($controllerName, $actionName, $info){
+    public function saveApiToWeb($controllerName, $actionName, $apiResult){
+        if(!is_string($controllerName) || !is_string($actionName)){
+            throw new \InvalidArgumentException("controllerName and actionName参数必须是字符串类型");
+        }
+        if(!is_array($apiResult) && is_null(@json_decode($apiResult, true))){
+            throw new \InvalidArgumentException('apiResult参数不是一个有效数组或JSON数据');
+        }
+        if(!is_array($apiResult)){
+            $apiResult = json_decode($apiResult, true);
+        }
+        $actionIds = $controllerName.'_'.$actionName;
+        $fileContent = self::fileContentReadHandle($actionIds);
+
+        $apiParams = $fileContent[$actionIds]['params'];
+        $paramsInfo = "";
+        if(!empty($apiParams)){
+            foreach ($apiParams as &$v) {
+                $paramsInfo .= "|".$v."|string|无|\n";
+            }
+        }else{
+            $paramsInfo .= "|无|无|无|\n";
+        }
+        $paramsMK = "\n**简要描述：**\n- ".$fileContent[$actionIds]['desc']."\n\n**请求URL：**\n- ` ".$this->projectUrl." `\n\n**请求方式：**\n- ".$fileContent[$actionIds]['method']."\n\n**参数：**\n\n|参数名|类型|说明|\n|:----|:-----|-----|\n".$paramsInfo."**返回示例**\n";
+
+//        $apiResult = json_encode($apiResult, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+//        $dataParams = '';
+//        $apiMk = $paramsMK."```\n".$apiResult."\n```\n**返回参数说明**\n\n|参数名|类型|说明|\n|:-----|:-----|-----|\n".$dataParams;
+//        $resultParamNameArr = [];
+//        foreach ($apiResult as $k => $v) {
+//            $resultParamNameArr[$k]['param_name'] = $k;
+//            $resultParamNameArr[$k]['param_type'] = gettype($v);
+//            if(is_array($v)){
+//                foreach ($v as $k1 => $v1) {
+//                    $resultParamNameArr[$k]['name'] = '---'.$k1;
+//                    $resultParamNameArr[$k]['param_type'] = gettype($v);
+//                }
+//            }
+//        }
+        $resultParamNameArr = self::resultArrayHandle($apiResult);
+        $resultParamNameArr = self::resultArrayTransform($resultParamNameArr, $resultParamNameArr);
+//        array_walk($resultParamNameArr, 'resultArrayTransform');
+//        echo json_encode($resultParamNameArr);
+//        var_dump(json_encode($resultParamNameArr));
+//        var_dump($resultParamNameArr);
         $data = [
-            "api_key"      => "dde6ce61cea704a9407d167f077d3144756972081",
-            "api_token"    => "95e8b7b770ce5fdff34bb8a5fea8cc9b2053901276",
+            "api_key"      => $this->apiKey,
+            "api_token"    => $this->apiToken,
             "cat_name"     => $controllerName,
             "page_title"   => $actionName,
-            "page_content" => $info
+            "page_content" => $apiMk
         ];
-        doCurl($data, 'https://showdoc.fanrehai.top/server/index.php?s=/api/item/updateByApi');
+        $this->doCurl($data, $this->apiUrl);
     }
 
-    public function doCurl($data, $url, $isReturn = 0){
+    /**
+     * 文件读取操作
+     */
+    private function fileContentReadHandle($actionIds)
+    {
+        $apiLogs = fopen('./apilogs.txt', 'a+');
+        $str = "";
+        //每次读取 1024 字节
+        $buffer = 1024;
+        //循环读取，直至读取完整个文件
+        while(!feof($apiLogs)) {
+            $str .= fread($apiLogs, $buffer);
+        }
+        fclose($apiLogs);
+        $separator = '//----------------------------------------------//';
+        $str = explode($separator, $str);
+        $str = array_filter($str);
+
+        if(!empty($str)){
+            foreach ($str as &$v) {
+                $v = json_decode($v, true);
+            }
+            $str = array_column($str, NULL, 'id');
+
+            $str_ids = array_column($str, 'id');
+            if(!in_array($actionIds, $str_ids)){
+                throw new \InvalidArgumentException('请先调用saveApiToLog方法');
+            }
+        }
+
+        return $str;
+    }
+
+    /**
+     * 文件写入操作
+     */
+    private function fileContentWriteHandle($content)
+    {
+        $this->fileContentReadHandle();
+        $apiLogs = fopen('./apilogs.txt', 'a+');
+        $separator = '//----------------------------------------------//';
+        $content = json_encode($content, JSON_UNESCAPED_UNICODE);
+
+        fwrite($apiLogs,PHP_EOL.$content.PHP_EOL.$separator);
+        fclose($apiLogs);
+
+        return ;
+    }
+
+    /**
+     * 数组操作
+     */
+    private function resultArrayHandle($apiResult, $index = 0)
+    {
+        $index += 1;
+        $resultParamNameArr = [];
+        foreach ($apiResult as $k => $v) {
+            $resultParamNameArr[$k]['param_name'] = $k;
+            $resultParamNameArr[$k]['param_type'] = gettype($v);
+            if(is_array($v)){
+                if(count($v) == count($v,1)){
+                    $resultParamNameArr[$k]['children'] = self::resultArrayHandle($v, $index);
+                }elseif(is_numeric(key($v))){
+                    $v = $v[key($v)];
+                    $resultParamNameArr[$k]['children'] = self::resultArrayHandle($v, $index);
+                }else{
+                    $resultParamNameArr[$k]['children'] = self::resultArrayHandle($v, $index);
+                }
+                $resultParamNameArr[$k]['level'] = $index;
+                $resultParamNameArr = array_values($resultParamNameArr);
+            }
+            $resultParamNameArr = array_values($resultParamNameArr);
+        }
+
+        foreach ($resultParamNameArr as &$v) {
+            if(is_array($v) && count($v) == 1){
+                $v = array_shift($v);
+            }
+            if(!empty($v['children'])){
+                $level = '';
+                for ($i = 0; $i < $v['level']; $i++) {
+                    $level .= '-';
+                }
+                foreach ($v['children'] as &$v1) {
+                    $v1['param_name'] = $level.$v1['param_name'];
+                }
+                unset($v['level']);
+//                $place = array_search($v, $resultParamNameArr);
+//                var_dump($resultParamNameArr);
+//                array_splice($v, $place, 0, $v['children']);
+//                $v =
+//                unset($v['children']);
+            }
+        }
+        return $resultParamNameArr;
+    }
+
+    /**
+     * 数组转换
+     */
+    private function resultArrayTransform($data, $originData){
+        foreach ($data as &$v) {
+            if(!empty($v['children'])){
+                $place = array_search($v, $data);
+                array_splice($data, $place + 1, 0, $v['children']);
+                foreach ($v['children'] as &$v1) {
+                    if(!empty($v1['children'])){
+                        $v2[] = $v1;
+                        $v1 = self::resultArrayTransform($v2, $data);
+                    }
+                }
+                unset($v['children']);
+            }
+        }
+        foreach ($data as &$v) {
+            if(count($v) != count($v,1)){
+                $place = array_search($v, $data);
+                unset($data[$place]);
+                // 截取之后再填充
+                $arrSliceStart = array_slice($data, 0, $place);
+                $arrSliceEnd   = array_slice($data, $place);
+                foreach ($v as &$v1) {
+                    $arrSliceStart[$place] = $v1;
+                    $place += 1;
+                }
+                $data = array_merge($arrSliceStart, $arrSliceEnd);
+            }
+        }
+        return $data;
+    }
+
+    private function doCurl($data, $url){
         //初始化
         $curl = curl_init();
         //设置抓取的url
@@ -64,16 +323,10 @@ Class ApiDoc
         //执行命令
         $result = curl_exec($curl);
         $errs   = curl_error($curl);
-        Log::info($result);
-        Log::info($errs);
         if (curl_errno($curl)) {
             return curl_error($curl);
         }
         //关闭URL请求
         curl_close($curl);
-        //显示获得的数据
-        if($isReturn){
-            return $result;
-        }
     }
 }
